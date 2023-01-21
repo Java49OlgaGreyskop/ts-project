f6b8cd8a868fda86943803306ad2a811e354dbd6
Olga greyskop <o.greyskop@gmail.com>
2023-01-21 13:57:22 +0200

    CW#32 Employee WageEmployee

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Rectangle_1 = require("./Rectangle");
-const Square_1 = require("./Square");
-const CipherDecipher_1 = require("./CipherDecipher");
-const ShapesContainer_1 = require("./ShapesContainer");
-const shapes = new ShapesContainer_1.ShapesContainer([
-    new Rectangle_1.Rectangle(3, 4),
-    new Square_1.Square(5),
-    new ShapesContainer_1.ShapesContainer([new ShapesContainer_1.ShapesContainer([new Square_1.Square(10), new Square_1.Square(4)]),
-        new Rectangle_1.Rectangle(6, 4)])
-]);
-const totalSquare = shapes.getSquare();
-console.log(totalSquare);
-const cipherDecipher = new CipherDecipher_1.CipherDecipher(32, 127, 10);
-console.log(cipherDecipher.cipher("Hello World!"));
-console.log(cipherDecipher.decipher("Rovvy*ay|vn+"));
+const WageEmployee_1 = require("./WageEmployee");
+const employee = new WageEmployee_1.WageEmployee(123, "Vasya", 2000, "QA", 10000, 100, 50);
+console.log(employee.id);
+employee.basicSalary = 60000;
 //# sourceMappingURL=app.js.map
